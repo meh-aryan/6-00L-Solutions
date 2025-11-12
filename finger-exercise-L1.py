275b620c61397de1a8ad1d4763b8a6d16a02f762
meh-aryan <aryan.works@yahoo.com>
2025-11-12 21:59:11 +0530

    Add files via upload

---
+++ finger-exercise-L1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Created on Wed Nov 12 12:30:00 2025
+
+Finger Exercise Lecture 1 - https://ocw.mit.edu/courses/6-100l-introduction-to-cs-and-programming-using-python-fall-2022/pages/lecture-1-introduction/
+Time Spent: 00:02:00
+
+@author: Aryan Singh
+"""
+a = 50
+b = 20
+c = 30
+
+total = (a+b)*c
+
+print(total) #print type - int
---
+++ ps0.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Created on Wed Nov 12 12:00:00 2025
+
+Problem Set 0 - https://ocw.mit.edu/courses/6-100l-introduction-to-cs-and-programming-using-python-fall-2022/resources/mit6_100l_f22_ps0_pdf/
+Assignment 4.3.1
+Time Spent: 00:02:00
+
+@author: Aryan Singh
+"""
+import numpy
+
+x = 5
+y = 8
+
+z = x + y
+a = numpy.log2(z)
+
+print(a) #print type - float
